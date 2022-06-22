0601f35bffd401ae79715df9e0780acdced6424f
fagha <faridahmadghafari@yahoo.com>
2022-06-22 09:45:45 -0700

    New Class1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+			int a=2, b=8;
+			int sum = a+b;
+			
+			System.out.println(sum);
 
 	}
 
